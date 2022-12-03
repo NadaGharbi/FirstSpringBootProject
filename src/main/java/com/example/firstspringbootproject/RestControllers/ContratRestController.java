3551181383b6515b1c31488486b39357f91eab90
NadaGh1 <gharbi.nd@gmail.com>
2022-12-03 16:08:24 +0100

    update ContratRestController

--- src/main/java/com/example/firstspringbootproject/RestControllers/ContratRestController.java
+++ src/main/java/com/example/firstspringbootproject/RestControllers/ContratRestController.java
@@ -8,38 +8,39 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("contrats") //pr personnaliser l'acces au WS
 public class ContratRestController {
     //on va exposer les methodes du service contratService
     @Autowired
     private IContratService ics;
 
-    @PostMapping
-    Contrat ajouterContract(Contrat c){
+    @PostMapping("ajouterC") //exposer en tant que WS
+    Contrat ajouterContract(@RequestBody Contrat c){ //obj json de type Contrat
         return ics.addContract(c); //on appelle l'implementation du service
     }
 
-    @PutMapping
-    Contrat modifierContract(Contrat c){
+    @PutMapping("modifierC")
+    Contrat modifierContract(@RequestBody Contrat c){
         return ics.addContract(c); //on appelle l'implementation du ContratService
     }
 
-    @DeleteMapping
-    void supprimerContract(Contrat c){
+    @DeleteMapping("supprimerObjC")
+    void supprimerContract(@RequestBody Contrat c){
         ics.deleteContract(c);
     }
 
-    @DeleteMapping
-    void supprimerContractById(Long id){
+    @DeleteMapping("supprimerIdC/{id}") //doit etre le meme nom de la var
+    void supprimerContractById(@PathVariable Long id){
         ics.deleteContractById(id);
     }
 
-    @GetMapping
+    @GetMapping("afficherListC")
     List<Contrat> trouverAllContracts(){
         return ics.findAllContracts();
     }
 
-    @GetMapping
-    Contrat trouverById(Long id){
+    @GetMapping("afficherObjC") //@avec RequestParam ne rien ajouter dans l'uri
+    Contrat trouverById(@RequestParam Long id){
         return ics.findById(id);
     }
 }
