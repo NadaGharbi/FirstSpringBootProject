908c3c38542d29dab78edcfdcc02299696cbbd35
NadaGh1 <gharbi.nd@gmail.com>
2022-12-03 12:18:34 +0100

    update DepartementService & IDepartementService

--- src/main/java/com/example/firstspringbootproject/Services/DepartementService.java
+++ src/main/java/com/example/firstspringbootproject/Services/DepartementService.java
@@ -1,10 +1,44 @@
 package com.example.firstspringbootproject.Services;
 
+import com.example.firstspringbootproject.DAO.Entities.Contrat;
+import com.example.firstspringbootproject.DAO.Entities.Departement;
 import com.example.firstspringbootproject.DAO.Repositories.DepartementRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class DepartementService implements  IDepartementService{
 
     @Autowired
     private DepartementRepository DR;
+
+    @Override
+    public Departement addDepartement(Departement d) {
+        return DR.save(d);
+    }
+
+    @Override
+    public Departement updateDepartement(Departement d) {
+        return DR.save(d);
+    }
+
+    @Override
+    public void deleteDepartement(Departement d) {
+        DR.delete(d);
+    }
+
+    @Override
+    public void deleteDepartementById(Long id) {
+        DR.deleteById(id);
+    }
+
+    @Override
+    public List<Departement> findAllDepartements() {
+        return (List<Departement>) DR.findAll();
+    }
+
+    @Override
+    public Departement findById(Long id) {
+        return DR.findById(id).get();
+    }
 }
--- src/main/java/com/example/firstspringbootproject/Services/IDepartementService.java
+++ src/main/java/com/example/firstspringbootproject/Services/IDepartementService.java
@@ -1,5 +1,17 @@
 package com.example.firstspringbootproject.Services;
 
+import com.example.firstspringbootproject.DAO.Entities.Contrat;
+import com.example.firstspringbootproject.DAO.Entities.Departement;
+
+import java.util.List;
+
 public interface IDepartementService {
+
+    Departement addDepartement(Departement d);
+    Departement updateDepartement(Departement d);
+    void deleteDepartement(Departement d);
+    void deleteDepartementById(Long id);
+    List<Departement> findAllDepartements();
+    Departement findById(Long id);
 
 }
