9d35a319ac4e9dcef262d5f2e5e343d08434feeb
NadaGh1 <gharbi.nd@gmail.com>
2022-12-03 12:25:26 +0100

    update DetailsEquipeService & IDetailsEquipeService

--- src/main/java/com/example/firstspringbootproject/Services/DetailsEquipeService.java
+++ src/main/java/com/example/firstspringbootproject/Services/DetailsEquipeService.java
@@ -1,10 +1,43 @@
 package com.example.firstspringbootproject.Services;
 
+import com.example.firstspringbootproject.DAO.Entities.DetailsEquipe;
 import com.example.firstspringbootproject.DAO.Repositories.DetailsEquipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class DetailsEquipeService implements IDetailsEquipeService{
 
     @Autowired
     private DetailsEquipeRepository DER;
+
+    @Override
+    public DetailsEquipe addDetailsEquipe(DetailsEquipe DE) {
+        return DER.save(DE);
+    }
+
+    @Override
+    public DetailsEquipe updateDetailsEquipe(DetailsEquipe DE) {
+        return DER.save(DE);
+    }
+
+    @Override
+    public void deleteDetailsEquipe(DetailsEquipe DE) {
+        DER.delete(DE);
+    }
+
+    @Override
+    public void deleteDetailsEquipeById(Long id) {
+        DER.deleteById(id);
+    }
+
+    @Override
+    public List<DetailsEquipe> findAllDetailsEquipe() {
+        return (List<DetailsEquipe>) DER.findAll();
+    }
+
+    @Override
+    public DetailsEquipe findById(Long id) {
+        return DER.findById(id).get();
+    }
 }
--- src/main/java/com/example/firstspringbootproject/Services/IDetailsEquipeService.java
+++ src/main/java/com/example/firstspringbootproject/Services/IDetailsEquipeService.java
@@ -1,4 +1,15 @@
 package com.example.firstspringbootproject.Services;
 
+import com.example.firstspringbootproject.DAO.Entities.Contrat;
+import com.example.firstspringbootproject.DAO.Entities.DetailsEquipe;
+
+import java.util.List;
+
 public interface IDetailsEquipeService{
+    DetailsEquipe addDetailsEquipe(DetailsEquipe DE);
+    DetailsEquipe updateDetailsEquipe(DetailsEquipe DE);
+    void deleteDetailsEquipe(DetailsEquipe DE);
+    void deleteDetailsEquipeById(Long id);
+    List<DetailsEquipe> findAllDetailsEquipe();
+    DetailsEquipe findById(Long id);
 }
