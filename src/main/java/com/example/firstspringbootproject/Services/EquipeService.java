832da1ffb28f2087d6f44250bd50f305aeac49d9
NadaGh1 <gharbi.nd@gmail.com>
2022-12-03 12:29:44 +0100

    update EquipeService & IEquipeService

--- src/main/java/com/example/firstspringbootproject/Services/EquipeService.java
+++ src/main/java/com/example/firstspringbootproject/Services/EquipeService.java
@@ -1,9 +1,42 @@
 package com.example.firstspringbootproject.Services;
 
+import com.example.firstspringbootproject.DAO.Entities.Equipe;
 import com.example.firstspringbootproject.DAO.Repositories.EquipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 public class EquipeService implements IEquipeService{
     @Autowired
-    private EquipeRepository EqR;
+    private EquipeRepository EQR;
+
+    @Override
+    public Equipe addEquipe(Equipe e) {
+        return EQR.save(e);
+    }
+
+    @Override
+    public Equipe updateEquipe(Equipe e) {
+        return EQR.save(e);
+    }
+
+    @Override
+    public void deleteEquipe(Equipe e) {
+        EQR.delete(e);
+    }
+
+    @Override
+    public void deleteEquipeById(Long id) {
+        EQR.deleteById(id);
+    }
+
+    @Override
+    public List<Equipe> findAllEquipes() {
+        return (List<Equipe>) EQR.findAll();
+    }
+
+    @Override
+    public Equipe findById(Long id) {
+        return EQR.findById(id).get();
+    }
 }
--- src/main/java/com/example/firstspringbootproject/Services/IEquipeService.java
+++ src/main/java/com/example/firstspringbootproject/Services/IEquipeService.java
@@ -1,4 +1,16 @@
 package com.example.firstspringbootproject.Services;
 
+import com.example.firstspringbootproject.DAO.Entities.Contrat;
+import com.example.firstspringbootproject.DAO.Entities.Equipe;
+
+import java.util.List;
+
 public interface IEquipeService {
+
+    Equipe addEquipe(Equipe e);
+    Equipe updateEquipe(Equipe e);
+    void deleteEquipe(Equipe e);
+    void deleteEquipeById(Long id);
+    List<Equipe> findAllEquipes();
+    Equipe findById(Long id);
 }
