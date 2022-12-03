5b0b186724e203928a19cccc61ff5034e8536607
NadaGh1 <gharbi.nd@gmail.com>
2022-12-03 12:33:42 +0100

    update EtudiantService & IEtudiantService

--- src/main/java/com/example/firstspringbootproject/Services/EtudiantService.java
+++ src/main/java/com/example/firstspringbootproject/Services/EtudiantService.java
@@ -1,9 +1,42 @@
 package com.example.firstspringbootproject.Services;
 
+import com.example.firstspringbootproject.DAO.Entities.Etudiant;
 import com.example.firstspringbootproject.DAO.Repositories.EtudiantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 public class EtudiantService implements IEtudiantService{
     @Autowired
-    private EtudiantRepository EtR;
+    private EtudiantRepository ETR;
+
+    @Override
+    public Etudiant addEtudiant(Etudiant e) {
+        return ETR.save(e);
+    }
+
+    @Override
+    public Etudiant updateEtudiant(Etudiant e) {
+        return ETR.save(e);
+    }
+
+    @Override
+    public void deleteEtudiant(Etudiant e) {
+        ETR.delete(e);
+    }
+
+    @Override
+    public void deleteEtudiantById(Long id) {
+        ETR.deleteById(id);
+    }
+
+    @Override
+    public List<Etudiant> findAllEtudiants() {
+        return (List<Etudiant>) ETR.findAll();
+    }
+
+    @Override
+    public Etudiant findById(Long id) {
+        return ETR.findById(id).get();
+    }
 }
--- src/main/java/com/example/firstspringbootproject/Services/IEtudiantService.java
+++ src/main/java/com/example/firstspringbootproject/Services/IEtudiantService.java
@@ -1,4 +1,15 @@
 package com.example.firstspringbootproject.Services;
 
+import com.example.firstspringbootproject.DAO.Entities.Contrat;
+import com.example.firstspringbootproject.DAO.Entities.Etudiant;
+
+import java.util.List;
+
 public interface IEtudiantService {
+    Etudiant addEtudiant(Etudiant e);
+    Etudiant updateEtudiant(Etudiant e);
+    void deleteEtudiant(Etudiant e);
+    void deleteEtudiantById(Long id);
+    List<Etudiant> findAllEtudiants();
+    Etudiant findById(Long id);
 }
