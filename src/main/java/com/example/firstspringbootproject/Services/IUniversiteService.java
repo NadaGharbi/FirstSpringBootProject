f55ce2d108e299ca3447e431537e8f158b12389c
NadaGh1 <gharbi.nd@gmail.com>
2022-12-03 12:37:43 +0100

    update UniversiteService & IUniversiteService

--- src/main/java/com/example/firstspringbootproject/Services/IUniversiteService.java
+++ src/main/java/com/example/firstspringbootproject/Services/IUniversiteService.java
@@ -1,4 +1,16 @@
 package com.example.firstspringbootproject.Services;
 
+import com.example.firstspringbootproject.DAO.Entities.Contrat;
+import com.example.firstspringbootproject.DAO.Entities.Universite;
+
+import java.util.List;
+
 public interface IUniversiteService{
+
+    Universite addUniversite(Universite u);
+    Universite updateUniversite(Universite u);
+    void deleteUniversite(Universite u);
+    void deleteUniversiteById(Long id);
+    List<Universite> findAllUniversites();
+    Universite findById(Long id);
 }
--- src/main/java/com/example/firstspringbootproject/Services/UniversiteService.java
+++ src/main/java/com/example/firstspringbootproject/Services/UniversiteService.java
@@ -1,10 +1,43 @@
 package com.example.firstspringbootproject.Services;
 
+import com.example.firstspringbootproject.DAO.Entities.Universite;
 import com.example.firstspringbootproject.DAO.Repositories.UniversiteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class UniversiteService implements IUniversiteService{
 
     @Autowired
     private UniversiteRepository UR;
+
+    @Override
+    public Universite addUniversite(Universite u) {
+        return UR.save(u);
+    }
+
+    @Override
+    public Universite updateUniversite(Universite u) {
+        return UR.save(u);
+    }
+
+    @Override
+    public void deleteUniversite(Universite u) {
+        UR.delete(u);
+    }
+
+    @Override
+    public void deleteUniversiteById(Long id) {
+        UR.deleteById(id);
+    }
+
+    @Override
+    public List<Universite> findAllUniversites() {
+        return (List<Universite>) UR.findAll();
+    }
+
+    @Override
+    public Universite findById(Long id) {
+        return UR.findById(id).get();
+    }
 }
